5a281bdac7f613e72f6cb09eaeb4bbf6e3561805
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-05-02 11:55:54 +0200

    remove unused file

--- fixForFox.js
+++ fixForFox.js
@@ -1,14 +1,6 @@
 import fs from 'fs'
 import cp from 'child_process'
 
-// firefox does not support es6 modules, so we build an es5 version of content script for firefox
-
-// cp.execSync('npx tsc --noEmit && npx vite build --config vite.firefox.config.ts')
-// cp.execSync('cp dist/assets/index.*.js indexDist.js ')
-// cp.execSync('npx tsc --noEmit && npx vite build --config vite.config.ts')
-// cp.execSync('mv indexDist.js dist/src/pages/content/index.js')
-
-
 // firefox does not support "service_worker" in manifest.json, so we need to replace it with "background.scripts"
 
 const manifest = JSON.parse(fs.readFileSync('./dist/manifest.json'))
@@ -20,7 +12,7 @@ manifest.browser_specific_settings = {
     strict_min_version: '111.0'
   },
   gecko_android: {
-    id: 'addon-defillama@llama.fi',
+    id: 'addon-defillama-android@llama.fi',
     strict_min_version: '111.0'
   },
 }
